8c611a769bc0f92121ffc5d6c9bd7338cc0233a1
codscino <nonmerecordo.coscino@gmail.com>
2024-02-17 00:12:35 +0100

    hello world cloud fun

--- mobile/lib/appwrite/auth_api.dart
+++ mobile/lib/appwrite/auth_api.dart
@@ -3,6 +3,8 @@ import 'package:appwrite/models.dart';
 import 'package:scialla/appwrite/constants.dart';
 import 'package:flutter/widgets.dart';
 
+import 'dart:convert';
+
 enum AuthStatus {
   uninitialized,
   authenticated,
@@ -20,9 +22,9 @@ class AuthAPI extends ChangeNotifier {
   // Getter methods
   User get currentUser => _currentUser;
   AuthStatus get status => _status;
-  String? get username => _currentUser?.name;
-  String? get email => _currentUser?.email;
-  String? get userid => _currentUser?.$id;
+  String? get username => _currentUser.name;
+  String? get email => _currentUser.email;
+  String? get userid => _currentUser.$id;
 
   // Constructor
   AuthAPI() {
@@ -36,7 +38,25 @@ class AuthAPI extends ChangeNotifier {
         .setEndpoint(APPWRITE_URL)
         .setProject(APPWRITE_PROJECT_ID)
         .setSelfSigned();
-    account = Account(client);
+  }
+
+  trigFun() async {
+    //functions
+    final functions = Functions(client);
+
+    try {
+      final execution = await functions.createExecution(
+        functionId: FUN_STARTER,
+        //body: json.encode({ 'foo': 'bar' }),
+        //xasync: false,
+        //path: '/',
+        method: 'GET',
+        //headers: {'X-Custom-Header': '123'}
+      );
+      print(execution.responseBody);
+    } catch (e) {
+      print(e);
+    }
   }
 
   loadUser() async {
@@ -52,18 +72,39 @@ class AuthAPI extends ChangeNotifier {
   }
 
   Future<User> createUser(
-      {required String email, required String password }) async {
+      {required String email, required String password}) async {
     try {
       final user = await account.create(
-          userId: ID.unique(),
-          email: email,
-          password: password,
-          name: 'Simone G');
+          userId: ID.unique(), email: email, password: password, name: 'Jimmy');
+
       return user;
     } finally {
       notifyListeners();
     }
   }
+
+  /*createDocument() async {
+    try {
+      final response = await databases.createDocument(
+          databaseId: APPWRITE_DATABASE_ID,
+          collectionId: COLLECTION_USERINFO,
+          documentId: userID,
+          data: {
+            'name': name,
+            'surname': 'Pesce',
+            'country': 'Italy',
+            //'birthday': DateTime.now(),
+          });
+      if (response.data.isNotEmpty) {
+        print('andata bello');
+        //await listDocument();
+        //Navigator.pop(context);
+      }
+    } catch (e) {
+      print(e);
+      rethrow;
+    }
+  }*/
 
   Future<Session> createEmailSession(
       {required String email, required String password}) async {
